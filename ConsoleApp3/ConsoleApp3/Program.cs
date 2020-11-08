8d9ca39f1f03f13ca5b18ce31a01085525b45a2b
Apollion95 <ms051481@student.ath.edu.pl>
2020-11-08 15:59:15 +0100

    Commit

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApp3
 {
@@ -6,7 +7,47 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+            //  int[][] tablicaPoszarpana = new int[4][];
+            //  Console.Write(tablicaPoszarpana.Length[i][j])// GetLenght(0);
+
+            int[][] tablicaPoszarpana = new int[][]
+            {
+                new int[]{1,2,3,4,5},
+                new int[]{9,8},
+                new int[]{4,5,6,7}
+            };
+
+            int suma=0;
+            for (int i = 0; i < tablicaPoszarpana.Length; i++)
+            {
+                for (int j = 0; j < tablicaPoszarpana[i].Length; j++)
+                {
+                    suma += tablicaPoszarpana[i][j];
+                }
+            }
+            Console.WriteLine("Suma tablicy to " + suma + "");
+
+
+            Console.WriteLine("Podaj zdanie");
+            string zdanie = Console.ReadLine();
+
+            zdanie = char.ToUpper(zdanie[0]) + zdanie.Substring(1);
+
+            if (zdanie.EndsWith("."))
+            {
+                Console.WriteLine(zdanie);
+            }
+            else
+            {
+                StringBuilder builder = new StringBuilder(zdanie);
+                builder.Append(".");
+                Console.WriteLine(builder);
+            }
+
+
+
+
         }
     }
 }
